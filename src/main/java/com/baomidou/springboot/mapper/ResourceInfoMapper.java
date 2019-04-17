73a9ef61b74f68fea900219a23ba2eb135f172ae
李锦 <jin.li05@hand-china.com>
2019-04-17 22:45:19 +0800

    资源管理mapper和service接口

---
+++ src/main/java/com/baomidou/springboot/mapper/ResourceInfoMapper.java
@@ -0,0 +1,14 @@
+package com.baomidou.springboot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.springboot.entity.ResourceInfo;
+
+/**
+ * (ResourceInfo)表Mapper接口
+ *
+ * @author zhanhua.cheng
+ * @since 2019-04-14 18:10:25
+ */
+public interface ResourceInfoMapper extends BaseMapper<ResourceInfo> {
+
+}
---
+++ src/main/java/com/baomidou/springboot/service/IResourceInfoService.java
@@ -0,0 +1,14 @@
+package com.baomidou.springboot.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.springboot.entity.ResourceInfo;
+
+
+/**
+ * (ResourceInfo)资源明细表服务接口
+ *
+ * @author zhanhua.cheng
+ * @since 2019-04-14 18:10:09
+ */
+public interface IResourceInfoService extends IService<ResourceInfo> {
+}
