9a327e46f45f82223bce36f613f5dfb4d3c16d69
李锦 <jin.li05@hand-china.com>
2019-04-26 23:14:08 +0800

    文件上传接口第一次提交

---
+++ src/main/java/com/baomidou/springboot/controller/UploadController.java
@@ -0,0 +1,74 @@
+package com.baomidou.springboot.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.api.ApiController;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.springboot.entity.Users;
+import com.baomidou.springboot.utils.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/file")
+public class UploadController extends ApiController {
+    private static final Logger log = LoggerFactory.getLogger(UploadController.class);
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseUtil.returnStr("上传失败，未选择文件!", HttpStatus.PROCESSING);
+        }
+        String fileName = file.getOriginalFilename();
+        String filePath = "E:\\school\\temp\\";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            log.info("上传成功");
+            return ResponseUtil.returnStr("上传成功!", HttpStatus.OK);
+        } catch (IOException e) {
+            log.error(e.toString(), e);
+        }
+        return ResponseUtil.returnStr("上传失败!", HttpStatus.PROCESSING);
+    }
+
+    // 多文件上传 （在那时没有用）
+    @PostMapping("/multi/upload")
+    public ResponseEntity<String> multiUpload(List<MultipartFile> files) {
+        System.out.println("file:\n" + files + "\n\n\n\n\n");
+        String filePath = "E:\\school\\temp1\\";
+        for (int i = 0; i < files.size(); i++) {
+            MultipartFile file = files.get(i);
+            if (file.isEmpty()) {
+                String returnStr = "上传第" + (i++) + "个文件失败";
+                return ResponseUtil.returnStr(returnStr, HttpStatus.PROCESSING);
+            }
+            String fileName = file.getOriginalFilename();
+
+            File dest = new File(filePath + fileName);
+            try {
+                file.transferTo(dest);
+                log.info("第" + (i + 1) + "个文件上传成功");
+            } catch (IOException e) {
+                log.error(e.toString(), e);
+                String returnStr = "上传第" + (i++) + "个文件失败";
+                return ResponseUtil.returnStr(returnStr, HttpStatus.PROCESSING);
+            }
+        }
+        return ResponseUtil.returnStr("上传成功", HttpStatus.OK);
+    }
+
+}
